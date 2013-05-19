850ae971e0764b8db4cd182343066ca0734bcf84
isummer <isummer@localhost.(none)>
2013-05-19 11:52:56 +0800

    modify style.

--- action/user_action.py
+++ action/user_action.py
@@ -4,10 +4,14 @@ import webapp2
 from google.appengine.api import users
 from db.config_db import *
 
+
 class Login(webapp2.RequestHandler):
     def get(self):
         config = Config()
-        loginurl = users.create_login_url('/admin')
+        referer = self.request.headers.get('Referer')
+        if not referer:
+            referer = "";
+        loginurl = users.create_login_url(referer)
         self.redirect(loginurl)
 
 
